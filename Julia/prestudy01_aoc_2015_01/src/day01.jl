bcfd0e0b01004518ae984afd6c59793f9a8ef994
Martin Loesch <martin@martinloesch.net>
2024-11-27 00:19:25 +0100

    Cleaned up and structure Julia code for day 1.

--- Julia/prestudy01_aoc_2015_01/src/day01.jl
+++ Julia/prestudy01_aoc_2015_01/src/day01.jl
@@ -1,42 +1,78 @@
 module Day01
 
-    function load_puzzle(file_path)
-        file_content = read(file_path, String)
+function print_day_header(day, title)
+    println("")
+    line = "-- Day $day: $title ---"
+    println(line)
+    println("="^length(line))
+    println("")
+end
 
-        return file_content
+function print_part_header(id, title)
+    println("")
+    line = "Part $id: $title"
+    println(line)
+    println("-"^length(line))
+end
+
+
+function load_puzzle(file_path)
+    file_content = read(file_path, String)
+
+    return file_content
+end
+
+function part1(data)
+    print_part_header(1, "Finally reached floor")
+    final_floor = 0
+    for c in data
+        final_floor +=
+            if c == '('
+                1
+            elseif c == ')'
+                -1
+            else
+                0
+            end
     end
 
+    println("Ending up on floor: ", final_floor)
+end
+
+function part2(data)
+    print_part_header(2, "First time basement")
+
+    current = 0
+    pos = 1
+    for c in data
+        current +=
+            if c == '('
+                1
+            elseif c == ')'
+                -1
+            else
+                0
+            end
+
+        if (current == -1)
+            println("Reached floor -1 in step: ", pos)
+            break
+        end
+
+        pos += 1
+    end
+end
+
 # Specify the path to your file file_path = "example.txt" # Open the file and read its content as a string file_content = read(file_path, String) println(file_content) # Output: Hello, Julia!
-    greet() = println("Hello, world!")
+greet() = println("Hello, world!")
 
 end
 
 data = Day01.load_puzzle("D:/SYS/loesch/Dropbox/Advent_of_Code-2024.git/Julia/prestudy01_aoc_2015_01/src/puzzle.txt")
 
-# println(data)
+Day01.print_day_header(1, "Not Quite Lisp")
 
-myx = 0
-pos = 1
-reached_once = false
-for c in data
-    global reached_once
-    global myx
-    global pos
-    myx += 
-        if c == '('
-            1
-        elseif c == ')'
-            -1
-        else
-            0
-        end
-    
-    if (!reached_once && myx == -1)
-        println("Reached floor -1 in step ", pos)
-        # break
-        reached_once = true
-    end
-    pos += 1
-end
+Day01.part1(data)
 
-println("Ending up on floor: ", myx)
+Day01.part2(data)
+
