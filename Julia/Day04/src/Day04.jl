5483a9d31009f036d2151c0fa6a4dc617c4e7c76
Martin Loesch <martin@martinloesch.net>
2024-12-05 00:23:38 +0100

    Solved part 2 of day 4 in Julia.

--- Julia/Day04/src/Day04.jl
+++ Julia/Day04/src/Day04.jl
@@ -27,10 +27,36 @@ function part1(wg)
 
             for s in steps
                 if is_in_grid(wg, [r, c]+3*s)
-                    count += check(wg, [r, c], s)
+                    count += check_part1(wg, [r, c], s)
                 end
             end
             
+        end
+    end
+
+    println("Count of XMAS's: $count")
+end
+
+function part2(wg)
+    aoc.print_part_header(2, "Count X-MAS occurrences")
+
+    count = 0
+    for r = eachindex(wg)
+        if r == 1 || r == length(wg)
+            continue
+        end
+        
+        for c = eachindex(wg[r])
+            if c == 1 || c == length(wg[1])
+                continue
+            end
+
+            # println("Check for X at $r/$c :  $(wg[r][c])")
+            if wg[r][c] != 'A'
+                continue
+            end
+
+            count += check_part2(wg, [r, c])            
         end
     end
 
@@ -49,7 +75,7 @@ function is_in_grid(grid, pos)
     return true
 end
 
-function check(grid, pos, step)
+function check_part1(grid, pos, step)
     for i = eachindex(text)
         next = pos + i*step
         # println("Check pos: $next -> $(grid[next[1]][next[2]])")
@@ -61,6 +87,18 @@ function check(grid, pos, step)
     return 1
 end
 
+function check_part2(grid, pos)
+    d1 = grid[pos[1]-1][pos[2]-1]grid[pos[1]+1][pos[2]+1]
+    d2 = grid[pos[1]+1][pos[2]-1]grid[pos[1]-1][pos[2]+1]
+    # println("d1: $d1      d2: $d2")
+
+    if 'M' in d1 && 'S' in d1 && 'M' in d2 && 'S' in d2
+        return 1
+    end
+
+    return 0
+end
+
 end # module Day04
 
 aoc.print_day_header(4, "Ceres Search")
@@ -70,3 +108,5 @@ word_grid = Day04.load_data("puzzle.txt")
 # println(word_grid)
 
 Day04.part1(word_grid)
+
+Day04.part2(word_grid)
