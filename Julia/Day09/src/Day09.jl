a2eef9cdd654d356fecac1c26633f528f8285fc1
Martin Loesch <martin@martinloesch.net>
2024-12-14 17:32:53 +0100

    Solved part 2 of day 9 in Julia.

--- Julia/Day09/src/Day09.jl
+++ Julia/Day09/src/Day09.jl
@@ -8,7 +8,7 @@ mutable struct Pointer
     FileId::Int32
     ListPos::Int32
     Count::Int32
-    IsFile :: Bool
+    IsFile::Bool
 end
 
 function load_data(file_path)
@@ -18,7 +18,7 @@ function load_data(file_path)
 end
 
 function part1(files)
-    aoc.print_part_header(1, "New Checksum")
+    aoc.print_part_header(1, "New Checksum #1")
 
     cs = 0
     filepos = 0
@@ -74,6 +74,98 @@ function part1(files)
     println("Checksum: $cs")
 end
 
+struct File
+    Id::Int32
+    Size::Int32
+end
+
+mutable struct FillSlot
+    Remaining :: Int32
+    Files :: Vector{File}
+end
+
+function part2(files)
+    aoc.print_part_header(2, "New Checksum #2")
+
+    # move files from the end, if possible
+    moved = Set()
+    filled = Dict{Int32, FillSlot}()
+    rp = Pointer(length(files) ÷ 2, length(files), 1, true)
+    while rp.ListPos > 1
+        slot = find_free_enough_slot(rp.ListPos, files, filled)
+        if (slot > 0)
+            if haskey(filled, slot)
+                push!(filled[slot].Files, File((rp.ListPos - 1) ÷ 2, files[rp.ListPos]))
+                filled[slot].Remaining -= files[rp.ListPos]
+            else
+                filled[slot] = FillSlot(files[slot] - files[rp.ListPos], [File((rp.ListPos - 1) ÷ 2, files[rp.ListPos])])
+            end
+            push!(moved, rp.ListPos)
+        end
+        rp.ListPos -= 2
+    end
+    # println("Filled: $filled")
+    # println("Moved: $moved")
+
+    # calculate checksum
+    cs = 0
+    filepos = 0
+    is_file = true
+    for pos ∈ eachindex(files)
+        if is_file
+            if pos in moved
+                # println("Skipping original #$((pos-1)÷2) @$pos")
+                filepos += files[pos]
+                is_file = !is_file
+                continue
+            end
+            id = (pos-1) ÷ 2
+            # println("Processing #$id @$pos")
+            for i ∈ 1:files[pos]
+                cs += filepos * id
+                # println("  + $filepos * $id (filepos*id)")
+                filepos += 1
+            end
+        else
+            if haskey(filled, pos)
+                for f in filled[pos].Files
+                    # println("Working on $f")
+                    # println("Processing #$(f.Id) @$pos")
+                    for i ∈ 1:f.Size
+                        cs += filepos * f.Id
+                        # println("  + $filepos * $(f.Id) (filepos*id)")
+                        filepos += 1
+                    end
+                end
+                filepos += filled[pos].Remaining
+            else 
+                filepos += files[pos]
+            end
+        end
+
+        is_file = !is_file
+    end
+
+    println("Checksum: $cs")
+end
+
+function find_free_enough_slot(file, files, filled)
+    len = files[file]
+    for s ∈ 2:2:(file-1)
+        if haskey(filled, s)
+            if filled[s].Remaining >= len
+                return s
+            end
+        else
+            if files[s] >= len
+                return s
+            end
+        end
+    end
+
+    return 0
+end
+
 end # module Day09
 
 # files = Day09.load_data("test.txt")
@@ -83,3 +175,5 @@ files = Day09.load_data("puzzle.txt")
 aoc.print_day_header(9, "Disk Fragmenter")
 
 Day09.part1(files)
+
+Day09.part2(files)
