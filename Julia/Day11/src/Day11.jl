395fc31dce40ac9d9761f5d06a2bb0e4ac6e8179
Martin Loesch <martin@martinloesch.net>
2024-12-23 17:43:12 +0100

    Solved part 2 of day 11 in Julia.

--- Julia/Day11/src/Day11.jl
+++ Julia/Day11/src/Day11.jl
@@ -35,6 +35,78 @@ function part1(pebbles)
     println("#Pebbles: $res")
 end
 
+function part2(pebbles)
+    aoc.print_part_header(2, "Pebble count after #75")
+
+    seen = Set()
+    expansions = Dict()
+    todo = []
+    for p in pebbles
+        if !(p in seen)
+            push!(todo, p)
+            push!(seen, p)
+        end
+    end
+    while length(todo) > 0
+        p = popfirst!(todo)
+        exp = expand_pebble_part2(p)
+        expansions[p] = exp
+        for e in exp
+            if !(e in seen)
+                push!(todo, e)
+                push!(seen, e)
+            end
+        end
+    end
+    println("Number of different pebbles: $(length(expansions))")
+
+    d = Dict()
+    for p in pebbles
+        if haskey(d, p)
+            d[p] += 1
+        else
+            d[p] = 1
+        end
+    end
+    for i ∈ 1:75
+        d = blink_part2(d, expansions)
+        # println("Step $i:\n$d")
+    end
+    res = sum(values(d))
+
+    println("Number of pebbles after 75 blinks: $res")
+end
+
+function expand_pebble_part2(p)
+    # println("Called expand for $p")
+    s = string(p)
+    if p == 0
+        return [1]
+    elseif length(s) % 2 == 0
+        middle = length(s) ÷ 2
+        p1, p2 = s[1:middle], s[middle+1:end]
+        return [parse(Int32, p1), parse(Int32, p2)]
+    else
+        return p * 2024
+    end
+end
+
+function blink_part2(pebble_set, pebble_expansions)
+    res = Dict()
+
+    for k in keys(pebble_set)
+        for e in pebble_expansions[k]
+            if haskey(res, e)
+               res[e] += pebble_set[k] 
+            else
+                res[e] = pebble_set[k] 
+            end
+        end
+    end
+
+    res
+end
+
 end # module Day11
 
 
@@ -50,4 +122,4 @@ aoc.print_day_header(11, "Plutonian Pebbles")
 
 Day11.part1(pebbles)
 
-# Day06.part2(pebbles)
+Day11.part2(pebbles)
